f9c5f3019891622a84c727adfcae032c2887a22c
Rupesh Bhasima <45381034+RoopsBhasima@users.noreply.github.com>
2019-08-01 11:24:15 +0545

    fibonacci with memoization

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,7 @@
+fibo_cache={}
 def fibo(n):
+	if n in fibo_cache:
+		return fibo_cache[n]
 	if n==1:
 		return 1
 	elif n==2:
@@ -6,5 +9,8 @@ def fibo(n):
 	else:
 		return fibo(n-1)+fibo(n-2)
 
-for i in range(1,10):
+	fibo_cache[n]=value
+	return value
+
+for i in range(1,100):  #memoization
 	print(fibo(i))
